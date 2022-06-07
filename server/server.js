01e64bf09ad6912a6533f4f5d5d7c24244c96007
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2022-06-07 13:59:19 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const postRoutes = require("./routes/posts");
 const commentRoutes = require("./routes/comment");
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  //origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
